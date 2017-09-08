4cc39de01246e7acc743a22b6bcfbdfba50b146a
Tim Freeman <d.timothy.freeman@gmail.com>
2017-09-08 16:00:32 -0400

    done

--- js/github_client.js
+++ js/github_client.js
@@ -1,10 +1,34 @@
 //define functions here
 var createGist = function(file_name, content, description, token){
-
+	let newUrl = 'https://api.github.com/gists' + '?access_token=' + token.toString()
+	$.ajax({
+		url: newUrl,
+		type: 'POST',
+		data: JSON.stringify({
+			'description': description,
+			 'public': true,
+			  'files':{
+			  [file_name]:{
+			  	'content': content}
+			  }
+			}),
+		success: function(data) {
+			myGists(data.owner.login, token)
+		}
+	})
 };
 
 var myGists = function (username, token){
-
+	var newUrl = 'https://api.github.com/users/' + username + '/gists' + '?access_token=' + token.toString()
+	$.ajax({
+		url: newUrl,
+		success: function(data) {
+			let rendered = data.map(function(gist) {
+				return '<a href="'+ gist.html_url + '">' + gist.description + '</a><br>'
+			})
+			document.getElementById('my-gists').innerHTML = rendered
+		}
+	})
 };
 
 var bindCreateButton = function() {
@@ -13,4 +37,12 @@ var bindCreateButton = function() {
 };
 
 $(document).ready(function(){
+	const token = document.getElementById("personal-token")
+	const fileName = document.getElementById("file-name")
+	const description = document.getElementById("description")
+	const content = document.getElementById("content")
+	$('#form-submit').on('click', function(event){
+		event.preventDefault()
+		createGist(fileName.value, content.value, description.value, token.value)
+	})
 });
